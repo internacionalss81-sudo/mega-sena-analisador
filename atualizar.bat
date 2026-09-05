6b3ffbc2b73086f523555eae8d6d7db17efde6f2
internacionalss81-sudo <internacionalss81@gmail.com>
2026-09-05 19:03:19 -0300

    Update atualizar.bat

--- atualizar.bat
+++ atualizar.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Salvando e enviando alteracoes para o GitHub...
 git add .
-set /p commit_message="Digite a descricao da atualizacao: "
+set /p commit_message="C:\Users\lindomarr\Downloads\MegaAnalyzer_GitHub_APK: "
 git commit -m "%commit_message%"
 git push origin main
 echo.
